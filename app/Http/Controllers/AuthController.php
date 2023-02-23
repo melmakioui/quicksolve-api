54c677cdb703311cc35a269596f1907db8855a66
Alan Skowronek <alanskowronek1414@gmail.com>
2023-02-23 20:47:42 +0100

    añadida condicion token.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,7 @@ class AuthController extends Controller
     public function handle(Request $request, Closure $closure){
         $token = $request->header("Authorization");
         $tokenParsed = explode(" ", $token);
+        if ($tokenParsed == null) return response()->json(["message" => "no hay token válido."], 401);
         $rq = Http::withBody($tokenParsed[1], 'text/plain')->post('http://spring-qs:8080/token/validar');
         return $rq->status() == 200 ? $closure($request) : response()->json(["message" => "No estas autorizado."], 401);
     }
